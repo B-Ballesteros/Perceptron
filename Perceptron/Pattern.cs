cd3019ab3ada0ce278592d817588db579e6f2843
Benjamin Ballesteros <jballesteros.mtz@gmail.com>
2018-08-18 17:44:40 -0500

    Added Pattern class to handle potential inputs and outputs for NN activation an training

---
+++ Perceptron/Pattern.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bballesteros.PNN
+{
+    public class Pattern
+    {
+        public double[] Inputs { get; private set; }
+        public double[] Outputs { get; private set; }
+
+        public Pattern() { }
+
+        public Pattern(int inputDimensions, int outputDimensions = 0)
+        {
+            if (inputDimensions < 0 || outputDimensions < 0)
+            {
+                throw new ArgumentOutOfRangeException("Invalid Value.\n The dimensions should be a positive integer.");
+            }
+            if (inputDimensions == 0)
+            {
+                throw new ArgumentException("InputDimension can't be 0. ");
+            }
+            Inputs = new double[inputDimensions];
+            if (outputDimensions != 0)
+            {
+                Outputs = new double[outputDimensions];
+            }
+        }
+
+        public Pattern(double[] inputs, double[] outputs = null)
+        {
+            Inputs = inputs ?? throw new ArgumentException("Inputs can't be null.");
+            Outputs = outputs;
+        }
+    }
+}
